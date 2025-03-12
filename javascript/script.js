daa107d9e9448fdbc12386d08e625c6771e72616
SeuNomeDeUsuario <seuemail@example.com>
2025-03-12 11:02:23 -0300

    estudo js

---
+++ javascript/script.js
@@ -0,0 +1,52 @@
+// criando variáveis
+
+let idade = 20
+
+console.log(idade)
+// console.log(idade + 2)
+// console.log(200)
+
+// // criando variáveis com string
+// let nome = "Lucas"
+// let nome2 = "90"
+// console.log(nome + 10)
+
+// // criando variáveis com boolean
+// let logado = true
+
+// // array e objetos
+// let ingredientes = ["ovo", "acucar", "farinha", "leite"]
+
+// console.log(ingredientes[2])
+
+// let personagem = {
+//     nome: "maya",
+//     idade: 19, 
+//     forca: 800,
+//     nivel: 10
+// }
+
+// console.log(personagem.nome)
+
+// /* funçoes- elementos:
+// - input
+// - processamento
+// - output
+// */
+
+// function somar(a, b){
+//     let resultado = a+b;
+//      return resultado
+// }
+
+// let x = somar(15, 5) //o resultado sera armazenado em x
+// console.log(x)
+
+// // arrowfunction
+// let somar2 = (a, b) =>{
+//     let resultado = a+b;
+//     return resultado
+// }
+
+// let y = somar2(2, 7)
+// console.log(y)
